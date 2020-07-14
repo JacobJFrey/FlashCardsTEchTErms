f5dfbea6a2097d323bc65d5f73b257ac9fdbd2ee
Jacob Frey <jacobjfrey@gmail.com>
2020-07-13 23:22:12 -0400

    Initial Stages of Flash Cards with 1 exmaple

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("definition");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
