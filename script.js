ea71029da2d20db2e0a208052a34e3a9ce383055
Jacob Frey <jacobjfrey@gmail.com>
2020-07-17 09:49:45 -0400

    Steve Mode Development

--- script.js
+++ script.js
@@ -42,6 +42,25 @@ function collectionAPITerms(){
   return flashStack;
 }
 
+function collectionAPITerms(){
+  let flashStack = [];
+  flashStack.push({term: "API", definition: "Application Program Interface"});
+  flashStack.push({term: "CRUD", definition: "Creat, Read, Update, Delete"});
+  flashStack.push({term: "DNS", definition: "Domain Name System"});
+  flashStack.push({term: "DTO", definition: "Data Transfer Object"});
+  flashStack.push({term: "HTTP", definition: "HyperText Transfer Protocol"});
+  flashStack.push({term: "HTTPS", definition: "HyperText Transfer Protocol Secure"});
+  flashStack.push({term: "JAR", definition: "Java ARchive"});
+  flashStack.push({term: "JWT", definition: "Java Web Token"});
+  flashStack.push({term: "JSON", definition: "JavaScript Object Notation"});
+  flashStack.push({term: "MVC", definition: "Model View Controller"});
+  flashStack.push({term: "REST", definition: "REpresentational State Transfer"});
+  flashStack.push({term: "URL", definition: "Uniform Resource Locator"});
+  flashStack.push({term: "URI", definition: "Uniform Resource Identifier"});
+  flashStack.push({term: "WAR", definition: "Web ARchive"});
+  return flashStack;
+}
+
 
 function shuffle() {
   let index = 1;
